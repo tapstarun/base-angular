d20937e6c9ff0ddbebb4706d214874bc58c44afb
tapstarun <taruntaneja1994@gmail.com>
2021-05-19 12:01:57 +0530

    header change

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService{
             "Access-Control-Allow-Headers": "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization" 
         };
 
-        return this.httpService.getData(newData,{Headers:header})
+        return this.httpService.getData(newData,[])
         .pipe(           
             tap((resData:any)=>{
                console.log('tap');
